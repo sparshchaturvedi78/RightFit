ccb83f364ffd7074e7740c323173b72ed8e31cc3
Sparsh Chaturvedi <sparshchaturvedi64@gmail.com>
2026-09-10 11:17:14 +0530

    DB Config Done

--- backend/src/main/java/com/rightFit/demo/DemoApplication.java
+++ backend/src/main/java/com/rightFit/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.rightFit.demo;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
